7e7563fc925929c4ff0357b7114f3b67965f18f5
Rujul-1105 <rujulmatta36@gmail.com>
2026-09-12 15:51:14 +0530

    change config

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 /**
  * Tailwind theme for Conviction.
@@ -131,7 +132,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 }
 
 export default config
